8c893d538631dd213ea76fd9f4cc0427aa5011a1
1am1goodtime@gmail.com <1am1goodtime@gmail.com>
2021-06-17 11:00:02 +0000

    bookers2-0617

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
     @book = Book.new
-
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/show'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   private
 
   def book_params
-    params.require(:list).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
  attachment :image
 
+
 end
